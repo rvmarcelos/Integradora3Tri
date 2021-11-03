1b3138bf969f3610df5ff2641a4d12d996714534
Marcelo Setubal <rvmarcelos@gmail.com>
2021-11-03 19:07:12 -0300

    Variaveis dummy, kmeans, qualidade de vida

--- codigo_integradora_3.R
+++ codigo_integradora_3.R
@@ -10,6 +10,15 @@ library(corrplot)
 
 ## TO DOs -----
 
+# Analise Kmeans
+
+# Melhorar matrix correlaÃ§Ã£o
+
+# Fazer Boxplots e outros grÃ¡ficos pertinentes
+
+
+# Tratar outliers
+
 # contar / tratar NAs nas colunas de tempo
 
 ## Import e Join Base de dados-----
@@ -33,7 +42,7 @@ geolocalization <- read.csv('olist_geolocation_dataset.csv')
 
 customers <- read.csv('olist_customers_dataset.csv')
 
-# Tentativa de fazer rápido, olhar depois
+# Tentativa de fazer rÃ¡pido, olhar depois
 # head(sellers)
 #
 # file_names_wd <- list.files( pattern = "*.csv", full.names = T)
@@ -50,7 +59,7 @@ df_olist <- orders %>%
   dplyr::full_join(customers, by = "customer_id") %>%
   dplyr::full_join(sellers, by = "seller_id")
 
-## Primeira visão da tabela-----
+## Primeira visÃ£o da tabela-----
 
 df_olist %>% select(ends_with("id")) %>%  glimpse()
 
@@ -60,7 +69,7 @@ df_olist %>% select(where(is.integer)) %>% glimpse()
 
 ## Limpeza da tabela-----
 
-# Tirando as colunas de identificação uma vez que foram juntadas e renomeando colunas order item id para number_order:
+# Tirando as colunas de identificaÃ§Ã£o uma vez que foram juntadas e renomeando colunas order item id para number_order:
 
 df_no_id <- df_olist %>% select(!ends_with("id")) %>% mutate(number_order = df_olist$order_item_id)
 
@@ -79,30 +88,53 @@ df_date <- df_no_id %>%
   select(!ends_with("timestamp")) %>%
   select(!order_approved_at)
 
-## Estimativa de dias de atraso e tempo decorrico
 
-df_delay <- df_date %>% mutate(delay_time = df_date$day_estimated - df_date$day_delivered_customer) %>%
-  mutate(delivery_time = df_date$day_delivered_customer -  df_date$day_purchase)
+## Substituir NA values por moda e mÃ©dia
 
-## Criar variável de alaviação alta ou baixa (high e low) para visualizar correlação de notas altas e outras variáveis
-
-df_review <- df_delay %>% mutate(review_high = ifelse(df_olist$review_score>=4, "high", "low"))
-
-## Criar variável tamanho do produto pois é mais relevante q ter três medidas (comprimento, altura e largura)
-
-df_dim <- df_review %>% select(!ends_with("cm")) %>%
-  mutate(product_dimensions_cm = df_olist$product_height_cm * df_olist$product_length_cm * df_olist$product_width_cm)
-
-## Substituir NA values por moda
-
-df_NA_chr <- df_dim %>% mutate(across(where(is.character),~replace_na(.x, max(.x , na.rm = TRUE))))
+df_NA_chr <- df_date %>%
+  mutate(across(where(is.character) & !starts_with("review"),~replace_na(.x, max(.x , na.rm = TRUE))))
 
 df_NA_numeric <- df_NA_chr %>% mutate(across(where(is.numeric),~replace_na(.x, mean(.x , na.rm = TRUE))))
 
-## Criar variável estado do vendedor é o mesmo do comprador
 
-df_ae <- df_NA_numeric %>% mutate(same_estate_cs = ifelse(df_olist$customer_state == df_olist$seller_state, 1, 0))
-## Analise Exploratória -----
+## Estimativa de dias de atraso e tempo decorrico
+
+df_delay <- df_NA_numeric %>% mutate(delay_time = as.numeric(df_NA_numeric$day_estimated - df_NA_numeric$day_delivered_customer)) %>%
+  mutate(delivery_time = as.numeric(df_NA_numeric$day_delivered_customer -  df_NA_numeric$day_purchase))
+
+## Criar variÃ¡vel de alaviaÃ§Ã£o alta ou baixa (high e low) para visualizar correlaÃ§Ã£o de notas altas e outras variÃ¡veis
+
+df_review <- df_delay %>% mutate(review_high = ifelse(df_delay$review_score>=4, "high", "low"))
+
+## Criar variÃ¡vel tamanho do produto pois Ã© mais relevante q ter trÃªs medidas (comprimento, altura e largura)
+
+df_dim <- df_review %>% select(!ends_with("cm")) %>%
+  mutate(product_dimensions_cm = df_review$product_height_cm * df_review$product_length_cm * df_review$product_width_cm)
+
+## Criar variÃ¡vel estado do vendedor Ã© o mesmo do comprador
+
+df_ae <- df_dim %>% mutate(same_estate_cs = ifelse(df_dim$customer_state == df_dim$seller_state, 1, 0))
+
+## Dummys
+
+Receita <- recipe(review_high ~ ., data = df_ae) %>%
+  step_select(-starts_with("review_comment"), -ends_with("city")) %>%
+  step_normalize(all_numeric_predictors()) %>%
+  step_dummy(all_nominal())
+
+
+preparado <- prep(Receita, df_ae)
+df_dummy <- bake(preparado, new_data = NULL)
+
+
+
+## VisualizaÃ§Ã£o pÃ³s tratamento
+
+#df_NA_chr %>% glimpse()
+
+df_ae %>% skimr::skim()
+
+## Analise ExploratÃ³ria -----
 # Variaveis categorica:
 
 df_ae %>% count(delivery_time) %>% arrange(desc(n))
@@ -121,7 +153,7 @@ fazer_graf_cat <- function(x){
   df_ae %>%
     ggplot(aes(.data[[x]], fill = review_high)) +
     geom_bar(position = 'dodge')
-
+  
 }
 fazer_list_cat <- function(x){
   df_ae %>% count(.data[[x]]) %>% arrange(desc(n)) %>% head()
@@ -149,15 +181,15 @@ fazer_graf_con <- function(x){
   df_ae %>%
     ggplot(aes(.data[[x]], fill = review_high)) +
     geom_histogram()
-
+  
 }
 graficos_con <- map(col_con, fazer_graf_con)
 
 summarise_cat <- map(col_con,fazer_list_cat )
 
-## Fazer uma matrix de correlação ("this is fine :,)")
+## Fazer uma matrix de correlaÃ§Ã£o ("this is fine :,)")----
 
-correlacao <- cor(df_ae %>% select(where(is.numeric)))
+correlacao <- cor(df_dummy %>% select(where(is.numeric)))
 
 corrplot(correlacao, method = 'color', order = 'alphabet')
 
@@ -168,5 +200,23 @@ df_ae %>% count(review_score)
 teste <- replace(NA, df_ae$review_score, mean(df_ae$review_score, na.rm = TRUE))
 df_semna <- replace
 
-## Processamento de dados  -----
+## Analise dos Sellers e Customers  -----
+
+#Sellers:
+
+df_ae %>% group_by(seller_zip_code_prefix) %>%
+  summarise(Media_atraso = mean(delay_time)) %>%
+  arrange(Media_atraso)
+
+df_ae %>% count(seller_state) %>%
+  arrange(desc(n))
+
+## Kmeans ----
+
+df_dummy %>% skimr::skim()
+df_ae  %>% skimr::skim()
+
+analisek <- kmeans(df_dummy %>% drop_na() %>% select(!starts_with("day")), centers = 4)
+
+df_ae %>% mutate(cluster = analisek$cluster)
 
